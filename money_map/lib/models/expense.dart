9b8a27b258b6c4a0b5b2b31d0eb5cb71ac0a5353
Mohamed Ahmed <3ab2wy1911@gmail.com>
2024-10-25 14:36:34 +0300

    Update Project

--- money_map/lib/models/expense.dart
+++
@@ -1,7 +0,0 @@
-class Expense {
-  final String name;
-  final String price;
-  final DateTime date;
-
-  Expense({required this.name, required this.price, required this.date});
-}
--- money_map/lib/models/income.dart
+++
@@ -1,7 +0,0 @@
-class Income{
-  final String name;
-  final String amount;
-  final DateTime date;
-
-  Income({required this.name, required this.amount, required this.date});
-}
